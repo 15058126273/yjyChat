46fb651722c767d4ef42d4cbbf97080981483835
yjy <1335823608@qq.com>
2018-05-02 23:13:04 +0800

    1.修改

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import requests
 import json
 
 
-def do_it():
+def do_it(nick):
 
     bot = Bot(cache_path=True)
     # 好友统计信息
     # print(bot.friends().stats())
     # my_friend = bot.mps().search(nick)[0]
-    # my_friend = bot.friends().search(nick)[0]
+    my_friend = bot.friends().search(nick)[0]
     # my_friend = bot.groups().search(nick)[0]
 
     # 打印来自其他好友、群聊和公众号的消息
     # 全局注册必须现在前面, 否则后面的注册会被覆盖
     @bot.register()
     def print_others(msg):
-        if msg.is_at():
+        if msg.is_at:
             print('received msg: ', msg)
             if msg.type == 'Text':
                 reply_msg = tuling(msg.text)
@@ -24,6 +24,13 @@ def do_it():
                 reply_msg = '抱歉, 我不明白你在说啥子'
             print('reply_msg:', reply_msg)
             return reply_msg
+
+    @bot.register(my_friend)
+    def print_point(msg):
+        if msg.is_at:
+            return "hello world"
+        else:
+            return '非服务对象'
 
 
 def tuling(content):
@@ -44,8 +51,8 @@ def tuling(content):
 
 
 if __name__ == '__main__':
-    do_it()
-    # do_it('娃娃机特工队')
+    # do_it()
+    do_it('啥')
 
     # print(tuling('玩玩玩'))
 
